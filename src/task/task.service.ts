8694724518089969346a876970a5fb27a105cdc6
RuamLeonel2024 <ruamleonel90@gmail.com>
2025-01-01 10:01:20 -0300

    Controller, Service and Module

---
+++ src/task/task.service.ts
@@ -0,0 +1,23 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+    
+    private tasks: TaskDto[] = [];
+    
+    create(task: TaskDto) {
+        this.tasks.push(task)
+        console.log(this.tasks)
+    }
+
+    findById(id: string): TaskDto {
+        const foundTask = this.tasks.filter(t => t.id === id);
+
+        if(foundTask.length) {
+            return foundTask[0]
+        }
+
+        throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND)
+    }
+}
